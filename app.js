32bf26494099137d3030ff343a069c0e3abfa847
realVato <44781061+realVato@users.noreply.github.com>
2019-04-08 19:54:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 var budgetController = (function (){
 
-    var x = 30;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return(add(b));
-        }
-    }
+    
 
 })();
 
@@ -22,12 +12,29 @@ var UIController = (function () {
 
 var controller = (function (budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget Controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('works');
+
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        } 
+    })
 
 })(budgetController, UIController);
