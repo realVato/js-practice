fac79a77bc5eaaee1dcf55fa1eaefaa521478c3d
realVato <44781061+realVato@users.noreply.github.com>
2019-07-18 21:49:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -101,6 +101,20 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            var fields;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -138,6 +152,10 @@ var controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
+
+        // 3.5. Clear fields
+        UICtrl.clearFields();
+
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
