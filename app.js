9df75546e4bd3dc8ef4a6a9458ec816ed2854347
realVato <44781061+realVato@users.noreply.github.com>
2019-07-20 03:30:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,26 @@ var budgetController = (function () {
 
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            // id = 6
+            // data.allItems[type][id]; 
+            // ids = [1 2 4  8]
+            // index = 3
+
+            var ids = data.allItems[type].map(function(cur) {
+                return cur.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         calculateBudget: function() {
 
             // calculate total income and expenses
@@ -242,16 +262,17 @@ var controller = (function (budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
 
             // 3. Update and show the new budget
 
         }
-        
+
     };
 
     return {
